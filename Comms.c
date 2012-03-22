c1ebac538a6a847330f7e2aa9893f9bddfb7d788
Myles Grant <myles@mylesgrant.com>
2012-03-21 22:07:04 -0700

    Init mavlink and send heartbeats -- untested

--- Comms.c
+++ Comms.c
@@ -12,6 +12,13 @@
 
 #include <mavlink.h>
 
+// Setup some mavlink vars
+mavlink_system_t mavlink_system;
+
+mavlink_message_t comms_msg;
+uint8_t comms_buf[MAVLINK_MAX_PACKET_LEN];
+
+// Setup some timers and callbacks
 static VirtualTimer vt_heartbeat;
 
 static void hb_interrupt(void *p) {
@@ -45,12 +52,20 @@ static UARTConfig uart2cfg = {
 	0
 };
 
+
 /*
  * Activates the serial driver 2
  * PA2(TX) and PA3(RX) are routed to USART2.
  */
 
 void CommsInit(void){
+
+	mavlink_system.sysid = 0;
+	mavlink_system.compid = MAV_COMP_ID_IMU;
+	mavlink_system.type = MAV_TYPE_QUADROTOR;
+	mavlink_system.mode = MAV_MODE_PREFLIGHT;
+	mavlink_system.state = MAV_STATE_STANDBY;
+	mavlink_system.nav_mode = MAV_AUTOPILOT_INVALID;
 
 	uartStart(&UARTD2, &uart2cfg);
 	palSetPadMode(GPIOA, 2, PAL_MODE_ALTERNATE(7)); // yellow wire on the FTDI cable
@@ -66,5 +81,10 @@ void CommsInit(void){
 
 void CommsHeartbeat(void){
 
-	//uartStartSend(&UARTD2, 13, "Starting...\r\n");
+	// Pack the message
+	mavlink_msg_heartbeat_pack(mavlink_system.sysid, mavlink_system.compid, &comms_msg, mavlink_system.type, mavlink_system.nav_mode, mavlink_system.mode, 0, mavlink_system.state);
+
+	// Copy the message to the send buffer
+	uint16_t len = mavlink_msg_to_send_buffer(comms_buf, &comms_msg);
+	uartStartSend(&UARTD2, len, comms_buf);
 }
