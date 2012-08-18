cb7a0e16a5fc43578cf0094f3a4fc5a861ccb55a
Myles Grant <myles@mylesgrant.com>
2012-08-17 21:15:58 -0700

    Fleshing out I2C

--- I2CSensor.c
+++ I2CSensor.c
@@ -10,5 +10,16 @@
 
 #include "I2CSensor.h"
 
+/* I2C interface #1 */
+static const I2CConfig i2cfg1 = {
+	OPMODE_I2C,
+	400000,
+	FAST_DUTY_CYCLE_2,
+};
+
 void I2CSensorInit(void){
+	/*
+	 * Starts I2C
+	 */
+	i2cStart(&I2CD1, &i2cfg1);
 }
