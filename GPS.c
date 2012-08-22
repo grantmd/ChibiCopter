478b25f9aeaf90514ba4bb3a618686277a188b83
Myles Grant <myles@mylesgrant.com>
2012-08-21 22:23:08 -0700

    New GPS NMEA parser state machine is done, now I *just* need all the logic for the various terms and sequences, plus testing

--- GPS.c
+++ GPS.c
@@ -5,10 +5,18 @@
     Talks to a GPS receiver over serial
 */
 
+#include <string.h>
+
 #include "ch.h"
 #include "hal.h"
 
 #include "GPS.h"
+
+gps_state_t gps_state; // Parsing state
+gps_data_t gps_data; // Valid lat/lon etc data
+gps_data_t gps_working_data; // Where we store data we're working on
+gps_satellites_t gps_satellites; // Valid satellite data
+gps_satellites_t gps_working_satellites; // Where we store satellite data we're working on
 
 /*
  * GPS setup
@@ -30,12 +38,9 @@ static msg_t GPS(void *arg){
 		// Read a byte off the GPS
 		uint8_t c = chnGetTimeout((BaseChannel *)&SD1, TIME_INFINITE);
 		chnPutTimeout(&SD2, c, TIME_INFINITE);
-		/*if (TinyGPS_encode(c)){
+		if (GPSParseChar(c)){
       		palSetPad(GPIOD, GPIOD_LED5); // red
-
-      		// Post a message
-      		chMBPost(&GPSmb, msg, TIME_IMMEDIATE);
-		}*/
+		}
 	}
 	return 0;
 }
@@ -51,5 +56,350 @@ void GPSInit(void){
 	palSetPadMode(GPIOA, 9, PAL_MODE_ALTERNATE(7)); // not currently connected
 	palSetPadMode(GPIOA, 10, PAL_MODE_ALTERNATE(7)); // incoming data from the GPS
 
+	GPSParserInit();
+
 	chThdCreateStatic(GPSWA, sizeof(GPSWA), NORMALPRIO, GPS, NULL);
+}
+
+/*
+ * Public functions for accessing our parsed and validated results in a thread-safe manner
+ */
+
+uint8_t hasFix(void){
+	chMtxLock(&gps_data.mtx);
+	uint8_t hasFix = gps_data.hasFix;
+	chMtxUnlock();
+
+	return hasFix;
+}
+
+int32_t getLatitude(void){
+	chMtxLock(&gps_data.mtx);
+	int32_t lat = gps_data.lat;
+	chMtxUnlock();
+
+	return lat;
+}
+
+int32_t getLongitude(void){
+	chMtxLock(&gps_data.mtx);
+	int32_t lon = gps_data.lon;
+	chMtxUnlock();
+
+	return lon;
+}
+
+int32_t getAltitude(void){
+	chMtxLock(&gps_data.mtx);
+	int32_t alt = gps_data.alt;
+	chMtxUnlock();
+
+	return alt;
+}
+
+unsigned long getCourse(void){
+	chMtxLock(&gps_data.mtx);
+	chMtxUnlock();
+}
+
+unsigned long getSpeed(void){
+	chMtxLock(&gps_data.mtx);
+	chMtxUnlock();
+}
+
+uint8_t getNumSatellites(void){
+	chMtxLock(&gps_satellites.mtx);
+	uint8_t numSatellites = gps_satellites.numVisible;
+	chMtxUnlock();
+
+	return numSatellites;
+}
+
+unsigned long getHdop(void){
+	chMtxLock(&gps_data.mtx);
+	chMtxUnlock();
+}
+
+/*
+ * Begin parsing logic
+ */
+
+/*
+ * Initializes our GPS/NMEA parser
+ */
+
+void GPSParserInit(void){
+	gps_state.sentence_type = GPS_SENTENCE_UNKNOWN;
+	gps_state.valid = 0;
+	gps_state.sentence_offset = gps_state.term_offset = 0;
+
+	// Init our mutexes on the data structs
+	chMtxInit(&gps_data.mtx);
+	chMtxInit(&gps_satellites.mtx);
+}
+
+/*
+ * Parse a character from incoming GPS data using a state machine
+ * Returns whether we have completed a valid sentence
+ */
+
+char GPSParseChar(char c){
+	switch (c){
+		case ',':
+			// end of term
+			_GPSParseTerm();
+			gps_state.sentence_offset++;
+			gps_state.term_offset = 0;
+			break;
+
+		case '\r':
+		case '\n':
+		case '*':
+			// end of sentence/term
+			_GPSParseTerm();
+			_GPSParseSentence();
+			gps_state.sentence_offset = gps_state.term_offset = 0;
+			break;
+
+		case '$':
+			// start of sentence
+			gps_state.sentence_type = GPS_SENTENCE_UNKNOWN;
+			gps_state.sentence_offset = gps_state.term_offset = 0;
+			gps_state.valid = 0;
+			break;
+
+		default:
+			// Regular data
+			gps_state.term[gps_state.term_offset++] = c;
+			break;
+	}
+
+	return gps_state.valid;
+}
+
+/*
+ * Parse a just-completed term and stash to working storage
+ */
+void _GPSParseTerm(void){
+	// First term is always sentence type
+	if (gps_state.sentence_offset == 0){
+		if (strncmp(gps_state.term, _GPGGA_TERM, gps_state.term_offset) == 0){
+			gps_state.sentence_type = GPS_SENTENCE_GPGGA;
+		}
+		else if (strncmp(gps_state.term, _GPRMC_TERM, gps_state.term_offset) == 0){
+			gps_state.sentence_type = GPS_SENTENCE_GPRMC;
+		}
+		else if (strncmp(gps_state.term, _GPGLL_TERM, gps_state.term_offset) == 0){
+			gps_state.sentence_type = GPS_SENTENCE_GPGLL;
+		}
+		else if (strncmp(gps_state.term, _GPGSA_TERM, gps_state.term_offset) == 0){
+			gps_state.sentence_type = GPS_SENTENCE_GPGSA;
+		}
+		else if (strncmp(gps_state.term, _GPGSV_TERM, gps_state.term_offset) == 0){
+			gps_state.sentence_type = GPS_SENTENCE_GPGSV;
+		}
+		else if (strncmp(gps_state.term, _GPVTG_TERM, gps_state.term_offset) == 0){
+			gps_state.sentence_type = GPS_SENTENCE_GPVTG;
+		}
+
+		return;
+	}
+
+	// Other terms vary depending on sentence type
+	switch (gps_state.sentence_type){
+		case GPS_SENTENCE_GPGGA:
+			switch (gps_state.sentence_offset){
+				case 1: // UTC
+					break;
+				case 2: // Latitude
+					break;
+				case 3: // N or S (North or South)
+					break;
+				case 4: // Longitude
+					break;
+				case 5: // E or W (East or West)
+					break;
+				case 6: // GPS Quality Indicator (fix type)
+					break;
+				case 7: // Number of satellites in view
+					break;
+				case 8: // Horizontal Dilution of precision (meters)
+					break;
+				case 9: // Antenna Altitude above/below mean-sea-level (geoid) (in meters)
+					break;
+				case 10: // Units of antenna altitude, meters
+					break;
+				case 11: // Geoidal separation
+					break;
+				case 12: // Units of geoidal separation, meters
+					break;
+				case 13: // Age of differential GPS data
+					break;
+				case 14: // Differential reference station ID
+					break;
+				default:
+					break;
+			}
+			break;
+
+		case GPS_SENTENCE_GPRMC:
+			switch (gps_state.sentence_offset){
+				case 1: // UTC
+					break;
+				case 2: // Status, V=Navigation receiver warning A=Valid
+					break;
+				case 3: // Latitude
+					break;
+				case 4: // N or S (North or South)
+					break;
+				case 5: // Longitude
+					break;
+				case 6: // E or W (East or West)
+					break;
+				case 7: // Speed over ground, knots
+					break;
+				case 8: // Track made good, degrees true
+					break;
+				case 9: // Date, ddmmyy
+					break;
+				case 10: // Magnetic Variation, degrees
+					break;
+				case 11: // E or W
+					break;
+				case 12: // FAA mode indicator (NMEA 2.3 and later)
+					break;
+				default:
+					break;
+			}
+			break;
+
+		case GPS_SENTENCE_GPGLL:
+			switch (gps_state.sentence_offset){
+				case 1: // Latitude
+					break;
+				case 2: // N or S (North or South)
+					break;
+				case 3: // Longitude
+					break;
+				case 4: // E or W (East or West)
+					break;
+				case 5: // UTC
+					break;
+				case 6: // Status A - Data Valid, V - Data Invalid
+					break;
+				case 7: // FAA mode indicator (NMEA 2.3 and later)
+					break;
+				default:
+					break;
+			}
+			break;
+
+		case GPS_SENTENCE_GPGSA:
+			switch (gps_state.sentence_offset){
+				case 1: // Selection mode: M=Manual, forced to operate in 2D or 3D, A=Automatic, 3D/2D
+					break;
+				case 2: // Mode (1 = no fix, 2 = 2D fix, 3 = 3D fix)
+					break;
+				case 3: // ID of 1st satellite used for fix
+					break;
+				case 4: // ID of 2nd satellite used for fix
+					break;
+				case 5: // ID of 3rd satellite used for fix
+					break;
+				case 6: // ID of 4th satellite used for fix
+					break;
+				case 7: // ID of 5th satellite used for fix
+					break;
+				case 8: // ID of 6th satellite used for fix
+					break;
+				case 9: // ID of 7th satellite used for fix
+					break;
+				case 10: // ID of 8th satellite used for fix
+					break;
+				case 11: // ID of 9th satellite used for fix
+					break;
+				case 12: // ID of 10th satellite used for fix
+					break;
+				case 13: // ID of 11th satellite used for fix
+					break;
+				case 14: // ID of 12th satellite used for fix
+					break;
+				case 15: // PDOP
+					break;
+				case 16: // HDOP
+					break;
+				case 17: // VDOP
+					break;
+				default:
+					break;
+			}
+			break;
+
+		case GPS_SENTENCE_GPGSV:
+			switch (gps_state.sentence_offset){
+				case 1: // total number of GSV messages to be transmitted in this group
+					break;
+				default: // The rest of this is fucking complicated
+					break;
+			}
+			break;
+
+		case GPS_SENTENCE_GPVTG:
+			switch (gps_state.sentence_offset){
+				case 1: // Track Degrees
+					break;
+				case 2: // T = True
+					break;
+				case 3: // Track Degrees
+					break;
+				case 4: // M = Magnetic
+					break;
+				case 5: // Speed Knots
+					break;
+				case 6: // N = Knots
+					break;
+				case 7: // Speed Kilometers Per Hour
+					break;
+				case 8: // K = Kilometers Per Hour
+					break;
+				case 9: // FAA mode indicator (NMEA 2.3 and later)
+					break;
+				default:
+					break;
+			}
+			break;
+
+		case GPS_SENTENCE_UNKNOWN:
+		default:
+			break;
+	}
+}
+
+/*
+ * Parse a just-completed sentence, verify checksum, and copy working storage to public storage in a thread-safe manner
+ * Sets gps_state.valid if sentence was valid and copied to public storage
+ */
+void _GPSParseSentence(void){
+	// TODO: Add checksumming and return early if invalid
+
+	switch(gps_state.sentence_type){
+		case GPS_SENTENCE_GPGGA:
+			chMtxLock(&gps_data.mtx);
+			// Copy data
+			chMtxUnlock();
+			break;
+		case GPS_SENTENCE_GPRMC:
+			break;
+		case GPS_SENTENCE_GPGLL:
+			break;
+		case GPS_SENTENCE_GPGSA:
+			break;
+		case GPS_SENTENCE_GPGSV:
+			break;
+		case GPS_SENTENCE_GPVTG:
+			break;
+		default:
+			// hmmmmm.....
+			break;
+	}
 }
--- GPS.h
+++ GPS.h
@@ -5,14 +5,78 @@
     Talks to a GPS over serial
 */
 
+#include "ch.h"
+#include "hal.h"
+
 #ifndef _GPS_H_
 #define _GPS_H_
 
-#define GPS_MAILBOX_SIZE 128
-extern Mailbox GPSmb;
-extern msg_t GPSMessage[GPS_MAILBOX_SIZE];
+// http://gpsd.berlios.de/NMEA.txt
+#define GPS_SENTENCE_UNKNOWN 0
+#define GPS_SENTENCE_GPGGA 1 // Time, position, and fix/satellite info
+#define GPS_SENTENCE_GPRMC 2 // Time, position, some speed/heading
+#define GPS_SENTENCE_GPGLL 3 // Time, position
+#define GPS_SENTENCE_GPGSA 4 // GPS DOP and active satellites
+#define GPS_SENTENCE_GPGSV 5 // Satellites in view
+#define GPS_SENTENCE_GPVTG 6 // Track made good and Ground speed
+
+#define _GPGGA_TERM   "GPGGA"
+#define _GPRMC_TERM   "GPRMC"
+#define _GPGLL_TERM   "GPGLL"
+#define _GPGSA_TERM   "GPGSA"
+#define _GPGSV_TERM   "GPGSV"
+#define _GPVTG_TERM   "GPVTG"
+
+// A struct to maintain decoding state
+// No mutex here since access is single-threaded
+typedef struct {
+	uint8_t sentence_type;
+	char term[15];
+	uint8_t sentence_offset;
+	uint8_t term_offset;
+	char valid;
+} gps_state_t;
+
+// A struct to hold our decoded gps data
+typedef struct {
+	uint8_t hasFix;
+	int32_t lat;
+	int32_t lon;
+	int32_t alt;
+	uint16_t eph;
+	uint16_t epv;
+	uint16_t vel;
+	uint16_t cog;
+	Mutex mtx;
+} gps_data_t;
+
+// A struct to hold satellite data
+typedef struct {
+	uint8_t numVisible; ///< Number of satellites visible
+	uint8_t prn[20]; ///< Global satellite ID
+	uint8_t elevation[20]; ///< Elevation (0: right on top of receiver, 90: on the horizon) of satellite
+	uint8_t azimuth[20]; ///< Direction of satellite, 0: 0 deg, 255: 360 deg.
+	uint8_t snr[20]; ///< Signal to noise ratio of satellite
+	Mutex mtx;
+} gps_satellites_t;
 
 // Public functions
 void GPSInit(void);
+void GPSParserInit(void);
+char GPSParseChar(char);
+
+uint8_t hasFix(void);
+int32_t getLatitude(void); // lat in hundred thousandths of a degree
+int32_t getLongitude(void); // long in hundred thousandths of a degree
+
+int32_t getAltitude(void); // signed altitude in centimeters (from GPGGA sentence)
+unsigned long getCourse(void); // course in last full GPRMC sentence in 100th of a degree
+unsigned long getSpeed(void); // speed in last full GPRMC sentence in 100ths of a knot
+uint8_t getNumSatellites(void); // number of satellites used in last full GPGGA sentence
+unsigned long getHdop(void); // horizontal dilution of precision in 100ths
+
+// Private functions
+void _GPSParseTerm(void);
+void _GPSParseSentence(void);
 
 #endif /* _GPS_H_ */
